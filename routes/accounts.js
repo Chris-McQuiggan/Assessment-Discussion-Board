ea6d7acf930b26623f0e6a7ece5c8ad53d690171
Chris-McQuiggan <chris@mcquiggan.info>
2019-07-26 09:51:42 +0100

    create account added

--- routes/accounts.js
+++ routes/accounts.js
@@ -14,15 +14,42 @@ router.get("/test", (req, res) => {
 });
 
 // @route   GET account/all
-// @desc    Get all items
+// @desc    Get all accounts
 // @access  Public
 router.get("/all", (req, res) => {
     Account.find({}, '-password')
         .then(accounts => {
             if (!accounts) {
-                res.status(404).send({ noItems: "There are no items" });
+                res.status(404).send({ noAccounts: "There are no accounts" });
             }
             res.json(acounts);
         })
-        .catch(err => res.status(404).send({ noItems: "There are no items" }));
+        .catch(err => res.status(404).send({ noAccounts: "There are no accounts" }));
 });
+
+// @route   POST account/createAccount
+// @desc    Create an account
+// @access  Public
+router.get("/createAccount", (req, res) => {
+
+    let val = Validator(req.body);
+  
+    if (val.isValid) {
+  
+      const newAccount = new Account({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+      });
+  
+      bcrypt.hash(req.body.password, 15)
+        .then((hash) => {
+          newAccount.password = hash
+          newAccount.save()
+          res.status(200).send({message: "Account Succesfully Created"})
+        })
+        .catch(err => res.status(555).json({ "Fault": `${err}` }))
+    } else {
+        res.status(404).send(val.errors);
+      }
+    });
