c700c12b3d62ea11a3fad6da7e9e40b018c3f28b
Chris-McQuiggan <chris@mcquiggan.info>
2019-07-26 09:30:02 +0100

    GET /all created

---
+++ routes/accounts.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const Account = require("../models/accounts");
+
+// @route   GET item/test
+// @desc    Tests route
+// @access  Public
+router.get("/test", (req, res) => {
+    res.json({
+        message: "test success"
+    });
+});
+
+// @route   GET account/all
+// @desc    Get all items
+// @access  Public
+router.get("/all", (req, res) => {
+    Account.find({}, '-password')
+        .then(accounts => {
+            if (!accounts) {
+                res.status(404).send({ noItems: "There are no items" });
+            }
+            res.json(acounts);
+        })
+        .catch(err => res.status(404).send({ noItems: "There are no items" }));
+});
